0a0f1d59fd8abb57a90069334e759bc33c16639b
anish-narain <69715492+anish-narain@users.noreply.github.com>
2022-10-21 14:21:44 +0100

    uploads to task 2

---
+++ task2/counter_tb.cpp
@@ -0,0 +1,44 @@
+//Header files (not name Vcounter.h for module counter)
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    //instatiate the counter module as Vcounter
+    //this is the DUT (name of all generated files)
+    Vcounter* top = new Vcounter;
+    //turn on signal tracing and tell Verilator to
+    //dump the waveform data to counter.vcd
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("counter.vcd");
+
+    //initialize simulation inputs
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    //run simulation for many clock cycles
+    for (i=0; i<300; i++) {
+
+        //dump variables into VCD file and toggle clock
+        for (clk=0; clk<2; clk++) {
+            tfp->dump (2*i+clk);
+            top->clk = !top->clk;
+            top->eval ();
+        }
+        top->rst = (i<2 | i==8);
+        top->en = (i>4);
+        top->eval();
+        if (Verilated::gotFinish()) exit(0);
+    }
+    tfp->close();
+    exit(0);
+}
+
+
